fd886bba4f433eb389044ec2356a42172255a72d
matijabelec <mbelec@foi.hr>
2015-06-27 19:21:00 +0200

    updated config data

--- fw/config.php
+++ fw/config.php
@@ -5,5 +5,17 @@ include_once('db_config.php');
 
 define('BFW_AUTHOR', 'Matija Belec');
 define('BFW_AUTHOR_WEBSITE', 'matijabelec.com');
+define('BFW_CREATION_DATE', '27 Jun 2015');
+
+
+define('BFW_ROUTE_NOT_FOUND', -801);
+
+define('BFW_CONTROLLER_NOT_FOUND', -1001);
+define('BFW_CONTROLLER_RETURN_OK', -1401);
+define('BFW_CONTROLLER_RETURN_ERR', -1404);
+
+
+define('RET_OK', BFW_CONTROLLER_RETURN_OK);
+define('RET_ERR', BFW_CONTROLLER_RETURN_ERR);
 
 ?>
