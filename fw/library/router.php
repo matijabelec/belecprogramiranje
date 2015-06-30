5dd8398a9c011cfa1f0bf8ea32162d11bdc3ec05
matijabelec <mbelec@foi.hr>
2015-06-30 22:25:10 +0200

    Router class updated

--- fw/library/router.php
+++ fw/library/router.php
@@ -1,7 +1,15 @@
 <?php
 
 class Router {
-    
+    public static function run_controller() {
+        $ok = Routes_Manager::check_route($url);
+        
+        if($ok == true) {
+            // run controller
+        } else {
+            // show error
+        }
+    }
 }
 
 ?>
