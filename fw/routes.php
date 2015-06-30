37d852529447055e64c1e78644cc0c929af04f23
matijabelec <mbelec@foi.hr>
2015-06-30 22:24:50 +0200

    added class for Routes_Manager

--- fw/routes.php
+++ fw/routes.php
@@ -1,5 +1,19 @@
 <?php
 
-
+class Routes_Manager {
+    protected static $route_list = array();
+    
+    public static function initialize_routes($route_list) {
+        if(is_array($route_list) ) {
+            self::$route_list = $route_list;
+            return true;
+        }
+        return false;
+    }
+    
+    public static function check_route($url) {
+        return false;
+    }
+}
 
 ?>
